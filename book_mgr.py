9d785c921cf066474dc448169fbb2659a1801518
punagarwal0-ship-it <punagarwal0@gmail.com>
2025-11-23 23:39:28 +0530

    Update book_mgr.py

--- book_mgr.py
+++ book_mgr.py
@@ -1,92 +1,83 @@
-# book_mgr.py
-import files
-import my_dates
+# book_operations.py
+import file_manager
+import fine_calculator
 
-# b_list is the list of books
 def add_b(b_list):
-    id = input("Enter Book ID: ")
-    name = input("Enter Title: ")
+    id=input("Enter Book ID: ")
+    name=input("Enter Title: ")
     
-    # 0=id, 1=name, 2=borrowed?, 3=date
-    new_book = [id, name, "No", ""]
+    new_book=[id, name, "No", ""]
     b_list.append(new_book)
     
-    files.save_data(b_list)
+    file_manager.save_data(b_list)
     print("Added.")
 
 def issue_b(b_list):
-    id = input("Enter ID: ")
+    id=input("Enter ID: ")
     
-    flag = 0
+    flag=0
     for b in b_list:
-        if b[0] == id:
-            flag = 1
-            if b[2] == "Yes":
+        if b[0]==id:
+            flag=1
+            if b[2]=="Yes":
                 print("Already issued.")
             else:
-                d = input("Enter Due Date (Format DD/MM/YYYY): ")
+                d=input("Enter Due Date (Format DD/MM/YYYY): ")
                 
                 try:
-                    # manual check for date format
-                    # just testing if split works
-                    p = d.split('/')
-                    t_d = int(p[0])
-                    t_m = int(p[1])
-                    t_y = int(p[2])
+                    p=d.split('/')
+                    d_test=int(p[0])
+                    m_test=int(p[1])
+                    y_test=int(p[2])
                     
-                    # if code reaches here, date is ok
-                    b[2] = "Yes"
-                    b[3] = d
-                    files.save_data(b_list)
+                    b[2]="Yes"
+                    b[3]=d
+                    file_manager.save_data(b_list)
                     print("Done.")
                     
                 except:
                     print("Error: Date format wrong! Try again.")
                     print("----------------")
-                    # call function again
                     issue_b(b_list)
                     return
             break
             
-    if flag == 0:
+    if flag==0:
         print("Not found.")
 
 def return_b(b_list):
-    id = input("Enter ID: ")
+    id=input("Enter ID: ")
     
-    flag = 0
+    flag=0
     for b in b_list:
-        if b[0] == id:
-            flag = 1
-            if b[2] == "No":
+        if b[0]==id:
+            flag=1
+            if b[2]=="No":
                 print("Book is here only.")
             else:
-                d_ret = input("Return Date (Format DD/MM/YYYY): ")
-                d_due = b[3]
+                d_ret=input("Return Date (Format DD/MM/YYYY): ")
+                d_due=b[3]
                 
                 try:
-                    # calc fine
-                    diff = my_dates.get_overdue(d_ret, d_due)
+                    diff=fine_calculator.get_overdue(d_ret, d_due)
                     
-                    if diff > 0:
-                        fine = diff * 5
+                    if diff>0:
+                        fine=diff*5
                         print("Late by", diff, "days.")
                         print("Fine is", fine)
                     else:
                         print("No fine.")
                     
-                    # reset book
-                    b[2] = "No"
-                    b[3] = ""
-                    files.save_data(b_list)
+                    b[2]="No"
+                    b[3]=""
+                    file_manager.save_data(b_list)
                     
                 except:
                     print("Error: Date typed wrong. Try again.")
                     print("----------------")
-                    # restart process
                     return_b(b_list)
                     return
             break
             
-    if flag == 0:
+    if flag==0:
         print("Not found.")
